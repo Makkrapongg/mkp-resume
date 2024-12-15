66d1af3be8a4ec372a852ef6d8066066edf9c197
Makkrapongg <110651269+Makkrapongg@users.noreply.github.com>
2024-12-15 19:41:43 +0700

    edit contact

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Server Running"));
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
